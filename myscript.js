01ffca0c41d91f8280cd08b297dc4dcd0c04ee1c
Promise-Digitals <promiseedeh025@gmail.com>
2024-03-27 15:26:55 -0700

    Initial commit

---
+++ myscript.js
@@ -0,0 +1,30 @@
+function first() {
+    document.getElementById('fullscreen').src = 'images/Audi.jpeg';
+}
+function second() {
+    document.getElementById('fullscreen').src = 'images/BMW.jpeg';
+}
+function third() {
+    document.getElementById('fullscreen').src = 'images/cesar-perez.jpg';
+}
+function fourth() {
+    document.getElementById('fullscreen').src = 'images/Ferrari.jpg';
+}
+function fifth() {
+    document.getElementById('fullscreen').src = 'images/Honda.jpeg';
+}
+function sixth() {
+    document.getElementById('fullscreen').src = 'images/jay-pizzle.jpg';
+}
+function seventh() {
+    document.getElementById('fullscreen').src = 'images/jesse-zheng.jpg';
+}
+function eighth() {
+    document.getElementById('fullscreen').src = 'images/lamborghini.jpeg';
+}
+function ninth() {
+    document.getElementById('fullscreen').src = 'images/Mercedes-benz.jpeg';
+}
+function tenth() {
+    document.getElementById('fullscreen').src = 'images/Mitsubishi.jpg';
+}
